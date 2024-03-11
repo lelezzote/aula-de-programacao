bcb5dc6b28b51778ba0901647254ab5f96cd6c22
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-03-11 10:32:59 -0400

    exer3

---
+++ terceiro.js
@@ -0,0 +1,12 @@
+let primeiro = 'k';
+let segundo = 9;
+
+if(primeiro === segundo){
+    console.log("Os números são iguais!");
+} else if (primeiro > segundo){;
+    console.log ("O pimeiro número é maior");
+} else if(primeiro < segundo){
+    console.log("O segunfo número é maior");
+} else {
+    console.log("Não é possivel comparar!");
+}
