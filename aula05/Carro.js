8c804b10ceef9dc95d94117999236c53b89239a6
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-04-03 15:58:36 -0300

    atividade

---
+++ aula05/Carro.js
@@ -0,0 +1,27 @@
+class Carro {
+     constructor(marca, modelo, cor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+     }
+}
+
+let meuCarro = new Carro ("Toyota", "Hillux", "Preta");
+console.log("Marca do  carro:", meuCarro.marca);
+console.log("Modelo do  carro:", meuCarro.modelo);
+console.log("Cor do  carro:", meuCarro.cor);
+
+let meuCarro02 = new Carro ("Ferrari", "458", "Vermelha");
+console.log("Marca do  carro:", meuCarro02.marca);
+console.log("Modelo do  carro:", meuCarro02.modelo);
+console.log("Cor do  carro:", meuCarro02.cor);
+
+let meuCarro03 = new Carro ("Volswagem", "Fusca", "Azul");
+console.log("Marca do  carro:", meuCarro03.marca);
+console.log("Modelo do  carro:", meuCarro03.modelo);
+console.log("Cor do  carro:", meuCarro03.cor);
+
+let meuCarro04 = new Carro ("Chevrolet", "Bel Air", "Prata");
+console.log("Marca do  carro:", meuCarro04.marca);
+console.log("Modelo do  carro:", meuCarro04.modelo);
+console.log("Cor do  carro:", meuCarro04.cor);
---
+++ aula05/Pessoa.js
@@ -0,0 +1,7 @@
+class Pessoa{
+    constructor(nome, idade){
+    }
+    saudacao(){
+        console.log(`Olá, meu nome é ${this.nome} e tenho ${this.}`)
+    }
+}
